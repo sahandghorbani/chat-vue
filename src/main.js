95edc45f7dfa54677e4f3cff1ab18a7ed3667965
Sahand-home <sahandsepidar1@gmail.com>
2023-12-24 18:26:10 +0330

    hotfix : debug dev

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-console.log("dev chnaged");
-console.log("dev debuuged feature");
-console.log("dev relased feautre");
+console.log("dev changed");
+console.log("dev debugged feature");
+console.log("dev released feature");
 
 createApp(App).use(router).mount("#app");
