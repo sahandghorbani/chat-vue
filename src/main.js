68c6f42d1b0157c0aba42ab73483033786f5d91e
Sahand-home <sahandsepidar1@gmail.com>
2023-12-24 18:22:43 +0330

    dev added feature

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "./assets/main.css";
+console.log("dev chnaged");
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
