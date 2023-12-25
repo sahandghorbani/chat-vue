e8b65cfa3a12f90727e5e5187284f213e02dfe9b
Sahand-home <sahandsepidar1@gmail.com>
2023-12-26 00:48:43 +0330

    sdsd

--- src/main.js
+++ src/main.js
@@ -4,7 +4,5 @@ import router from "./router";
 
 import "./assets/main.css";
 console.log("dev chnaged");
-console.log("dev debuuged feature");
-console.log("dev relased feautre");
 
 createApp(App).use(router).mount("#app");
